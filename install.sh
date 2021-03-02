ff008c971bf4df10e2776754405511f43f6c129c
Stylix58 <63605602+Stylix58@users.noreply.github.com>
2021-03-02 15:46:37 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 echo "Installing Flame..."
+mkdir flame-inst
+cd flame-inst
 echo "Clone GitHub repository..."
 git clone https://github.com/Stylix58/flame
-cd flame
+cd /flame
 echo "Installing PyInstaller..."
 pip3 install pyinstaller
 echo "Compiling Flame..."
 pyinstaller --onefile --name flame -- main.py
 cd dist
-mv flame ../../flame/
-echo "Delete Flame's local Git repository..."
-cd ../../
-rm -rf ./flame/
+mv flame ../../../flame/
+cd ../../../
+echo "Remove local Git repository..."
+sudo rm -rf /flame
 echo "Installation finished!"
 echo "WARNING: You need to change Flame of directory."
 echo "For make this task, just type in this terminal (you need to have sudo rights): sudo mv flame /usr/bin/flame"
