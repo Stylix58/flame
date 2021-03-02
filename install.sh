fd429d7f2694e90cefef33187526c1068aa45c34
Stylix58 <63605602+Stylix58@users.noreply.github.com>
2021-03-02 15:51:08 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,10 +13,10 @@ pyinstaller --onefile --name flame -- main.py
 cd dist
 mv flame ../../../flame/
 cd ../../../
-echo "Remove local Git repository..."
-rm -rf /flame
 echo "Remove installation folder..."
 rm -rf /flame-inst
+echo "Remove local Git repository..."
+rm -rf /flame
 echo "Installation finished!"
 echo "WARNING: You need to change Flame of directory."
 echo "For make this task, just type in this terminal (you need to have sudo rights): sudo mv flame /usr/bin/flame"
